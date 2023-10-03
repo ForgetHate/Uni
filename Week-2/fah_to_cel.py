76240b2d60ea32c148a146375fbb55aa86ed4ea8
ForgetHate <iiderpzii@gmail.com>
2023-10-03 14:06:15 +0100

    Week 2 work + sorted into folder

---
+++ Week-2/fah_to_cel.py
@@ -0,0 +1,6 @@
+# This program converts Fahrenheit into Celcius #
+# Week 2, Exercise code #
+
+temp = float(input("Please input a temprature in Fahrenheit: "))
+cel = (temp - 32) * 5/9
+print(f"{temp} in Fahrenheit converted into Celsius is {cel} degrees")
