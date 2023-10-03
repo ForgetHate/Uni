4b244d48d4eb7f1cdaa3a69dc4f218bac0d8a5f6
ForgetHate <iiderpzii@gmail.com>
2023-10-03 14:13:58 +0100

    Week 2 work

---
+++ a_and_b.py
@@ -0,0 +1,13 @@
+# This code prints, adds and multiplies tow user input nunmbers #
+# Week 2, Exercise code #
+
+a = float(input("Please enter a number: "))
+b = float(input("Please enter another number: "))
+
+print(f"The value of 'a' was {a} and the value of 'b' was {b}")
+
+sum = a + b
+print(f"The sum of 'a' and 'b' is {sum}")
+
+prod = a * b
+print(f"The product of 'a' and 'b' is {prod}")
