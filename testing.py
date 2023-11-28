069d0947b187212985ef50a7158596524307ec17
ForgetHate <iiderpzii@gmail.com>
2023-11-28 13:02:17 +0000

    code

--- testing.py
+++ testing.py
@@ -1,23 +1,16 @@
 if __name__ == "__main__":
-    def recall():
-        area_calc = input(int('Please choice from one of the following: \n1. The area of a circle \n2. The area of a rectangle \n3. The area of a square \n4. The area of a Triangle \nEnter: '))
+    line = ''
+    racer = []
+    times = []
+    count = 0
 
-        if area_calc == "1":
-            # Circle Area
-            print('1')
-        elif area_calc == "2":
-            # Rectangle Area
-            print('2')
-        elif area_calc == "3":
-            # Square Area
-            print('3')
-        elif area_calc == "4":
-            # Triangle Area
-            print('4')
-        else:
-            recall()
-    
-    
-    recall()
-
-    
+    while line != "end":
+        line = input("> ")
+        split = line.split("::")
+        if len(split) == 2:
+            racer.append(split[0])
+            times.append(split[1])
+            count += 1
+    print(racer)
+    print(times)
+    print(f'Number of inputs {count}')
