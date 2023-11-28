8980c93cd8b2cd9adf20c4c4f05198185579e5fa
ForgetHate <iiderpzii@gmail.com>
2023-11-28 13:28:59 +0000

    racer

--- testing.py
+++ testing.py
@@ -1,16 +1,26 @@
+def getTime(seconds):
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    if minutes > 1:
+        return "%02d minutes, %02d seconds" % (minutes, seconds)
+    else:
+        return "%02d minute, %02d seconds" % (minutes, seconds)
+
 if __name__ == "__main__":
     line = ''
     racer = []
     times = []
-    count = 0
 
     while line != "end":
         line = input("> ")
         split = line.split("::")
         if len(split) == 2:
             racer.append(split[0])
-            times.append(split[1])
-            count += 1
-    print(racer)
-    print(times)
-    print(f'Number of inputs {count}')
+            times.append(int(split[1]))
+
+    averageTime = getTime(sum(times) / len(times))
+    fastestTime = getTime(min(times))
+    slowestTime = getTime(max(times))
+    winner = racer[times.index(min(times))]
+
+    print(f'Average Time: {averageTime} \nFastest Time: {fastestTime} \nSlowest Time: {slowestTime} \nWinner: {winner}')
